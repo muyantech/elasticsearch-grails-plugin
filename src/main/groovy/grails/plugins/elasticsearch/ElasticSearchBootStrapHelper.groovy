ea2031e8cfde127ad29f734f03ca176e2b05935a
Liu Xiang Qian <xiangqian.liu@muyan.cloud>
2020-11-06 05:09:40 +0800

    bulkIndexOnStartup in a separate thread to avoid grails startup too long
    Fix Transaction not found

--- src/main/groovy/grails/plugins/elasticsearch/ElasticSearchBootStrapHelper.groovy
+++ src/main/groovy/grails/plugins/elasticsearch/ElasticSearchBootStrapHelper.groovy
@@ -1,6 +1,7 @@
 package grails.plugins.elasticsearch
 
 import grails.core.GrailsApplication
+import grails.gorm.transactions.Transactional
 import grails.plugins.elasticsearch.mapping.MappingMigrationStrategy
 import grails.plugins.elasticsearch.util.ElasticSearchConfigAware
 import grails.plugins.elasticsearch.util.IndexNamingUtils
@@ -22,28 +23,35 @@ class ElasticSearchBootStrapHelper implements ElasticSearchConfigAware {
     ElasticSearchAdminService elasticSearchAdminService
     ElasticSearchContextHolder elasticSearchContextHolder
 
-    void bulkIndexOnStartup() {
-        def bulkIndexOnStartup = esConfig?.bulkIndexOnStartup
-        //Index Content
-        if (bulkIndexOnStartup == "deleted") { //Index lost content due to migration
-            LOG.debug "Performing bulk indexing of classes requiring index/mapping migration ${elasticSearchContextHolder.indexesRebuiltOnMigration} on their new version."
-            Class[] domainsToReindex = elasticSearchContextHolder.findMappedClassesOnIndices(elasticSearchContextHolder.indexesRebuiltOnMigration) as Class[]
-            elasticSearchService.index(domainsToReindex)
-        } else if (bulkIndexOnStartup) { //Index all
-            LOG.debug "Performing bulk indexing."
-            elasticSearchService.index(Collections.emptyMap()) // empty map is needed for static compiling
-        }
-        //Update index aliases where needed
-        MappingMigrationStrategy migrationStrategy = migrationConfig?.strategy ? MappingMigrationStrategy.valueOf(migrationConfig?.strategy as String) : MappingMigrationStrategy.none
-        if (migrationStrategy == MappingMigrationStrategy.alias) {
-            elasticSearchContextHolder.indexesRebuiltOnMigration.each { String indexName ->
-                int latestVersion = elasticSearchAdminService.getLatestVersion(indexName)
-                if(!migrationConfig?.disableAliasChange) {
-                    elasticSearchAdminService.pointAliasTo IndexNamingUtils.queryingIndexFor(indexName), indexName, latestVersion
-                }
-                elasticSearchAdminService.pointAliasTo IndexNamingUtils.indexingIndexFor(indexName), indexName, latestVersion
-            }
-        }
+  void bulkIndexOnStartup() {
+    Thread.start {
+      run()
     }
+  }
+
+  @Transactional(readOnly = true)
+  void run() {
+    def bulkIndexOnStartup = esConfig?.bulkIndexOnStartup
+    //Index Content
+    if (bulkIndexOnStartup == "deleted") { //Index lost content due to migration
+      LOG.debug "Performing bulk indexing of classes requiring index/mapping migration ${elasticSearchContextHolder.indexesRebuiltOnMigration} on their new version."
+      Class[] domainsToReindex = elasticSearchContextHolder.findMappedClassesOnIndices(elasticSearchContextHolder.indexesRebuiltOnMigration) as Class[]
+      elasticSearchService.index(domainsToReindex)
+    } else if (bulkIndexOnStartup) { //Index all
+      LOG.debug "Performing bulk indexing."
+      elasticSearchService.index(Collections.emptyMap()) // empty map is needed for static compiling
+    }
+    //Update index aliases where needed
+    MappingMigrationStrategy migrationStrategy = migrationConfig?.strategy ? MappingMigrationStrategy.valueOf(migrationConfig?.strategy as String) : MappingMigrationStrategy.none
+    if (migrationStrategy == MappingMigrationStrategy.alias) {
+      elasticSearchContextHolder.indexesRebuiltOnMigration.each { String indexName ->
+        int latestVersion = elasticSearchAdminService.getLatestVersion(indexName)
+        if(!migrationConfig?.disableAliasChange) {
+          elasticSearchAdminService.pointAliasTo IndexNamingUtils.queryingIndexFor(indexName), indexName, latestVersion
+        }
+        elasticSearchAdminService.pointAliasTo IndexNamingUtils.indexingIndexFor(indexName), indexName, latestVersion
+      }
+    }
+  }
 
 }
